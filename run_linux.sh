d93502c4e07d11bda94c07d0387aaf00dcecd560
Paul Alesius <paul@unnservice.com>
2015-11-11 12:43:21 +0100

    Fix gitignore and run_linux script

--- run_linux.sh
+++ run_linux.sh
@@ -5,6 +5,6 @@
 # Make boot.img
 external/mkbootimg.git/mkbootimg --cmdline "noinitrd i915.preliminary_hw_support=1 root=/dev/mmcblk0p14 selinux=0" --base 0x00100000 --kernel prebuilt/kernel --ramdisk prebuilt/initramfs-empty.img --dt prebuilt/dt.img -o prebuilt/boot-linux.img
 
-fastboot flash osloader prebuilt/kernel
+fastboot flash osloader prebuilt/bootx64.efi
 fastboot -S 10M boot prebuilt/boot-linux.img
 
